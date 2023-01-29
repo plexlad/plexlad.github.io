0353ba425bfdec25a62807f5a7aa86df47ecfe6d
Brock Smith <104398476+plexlad@users.noreply.github.com>
2023-01-29 04:03:45 -0700

    Used jQuery for improved syntax

--- nav-footer.js
+++ nav-footer.js
@@ -1,24 +1,17 @@
 /* Import the HTML for the nav menu and footer! */
 
-function includeHTML() {  // A function that loads the nav menu and footer into the current file
-	var xhttp, file, z, i, element;
-	z = document.getElementsByTagName("*");
-	for (i = 0; i < z.length; i++) {
-		element = z[i];
-		file = element.getAttribute("load-html");
-		if (file) {
-			xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4) {
-					if (this.status == 200) {element.innerHTML = this.responseText;}
-					if (this.status == 404) {element.innerHTML = "Page not found.";}
-					element.removeAttribute("load-html");
-					includeHTML();
-				}
-			}
-			xhttp.open("GET", file, true);
-			xhttp.send();
-			return;
+function includeHTML(fileloc) {  // A function that loads the nav menu and footer into the current file
+	$('body').append($(`<div id="my-content"></div>`));
+	var div = document.getElementById('my-content');
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {div.innerHTML = this.responseText;}
+			if (this.status == 404) {div.innerHTML = "Page not found.";}
 		}
 	}
+	xhttp.open("GET", fileloc, true);
+	xhttp.send();
+	return;
 }
